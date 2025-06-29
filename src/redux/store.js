fed3ab7fd1d456cd14706e26215d3504910af68d
nitin <nitingoley21@gmail.com>
2025-06-29 10:28:26 +0530

    fix deployment dependencies

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension"; 
+import thunk from "redux-thunk"; 
 import { authReducer } from "./reducers/auth.reducer";
 import { homeVideosReducer } from "./reducers/video.reducer";
+
 const rootReducer = combineReducers({
   auth: authReducer,
   homeVideos: homeVideosReducer
